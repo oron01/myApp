6c064c1a0f9d499a8071b9b454f0d76022b45664
Oron <136211946+oron01@users.noreply.github.com>
2025-09-12 16:59:34 +0300

    create an early prototype of the note structure, with scrolling and opening, and add some inner project page navigation functions, functioning but unclear the stage

--- public/js/projectHub.js
+++ public/js/projectHub.js
@@ -1,5 +1,5 @@
 import db from "/js/db.js"
-import {setUpSession,getLatestSessionToken,makeOutdated} from "/js/session.js"
+import {setUpSession,getLatestSessionToken,makeOutdated,isValid} from "/js/session.js"
 
 await setUpSession()
 const startingSessionToken = await getLatestSessionToken()
@@ -38,5 +38,73 @@ document.addEventListener('keydown',async e => {
     }
     )
 }
+
+let setCSS = () => {
+const bodyElement = document.querySelector('body');
+const mainContainerElement = document.querySelector(".mainContainer")
+
+bodyElement.style.setProperty('--pageCount', mainContainerElement.children.length);
+let pagesToMiddlePage = Math.floor(mainContainerElement.children.length / 2)
+document.documentElement.style.setProperty('--pagesToMiddleCount', pagesToMiddlePage);
+
+bodyElement.style.setProperty('--positionModifier', 0);
+    bodyElement.style.setProperty('--centerFixer', 0)
+
+
+let setMovementButton = (buttonClass,modifier) => {
+    
+    let mainContainer = document.querySelector(".mainContainer")
+    let button = document.querySelector(`.${buttonClass}`)
+    
+    button.parentElement.addEventListener("click",() => {
+    let currentPage = document.querySelector(".selectedPage")
+
+
+    mainContainer.classList.add("animatePageTransitions")
+    
+    let pageIndexModifier = Number(getComputedStyle(bodyElement).getPropertyValue('--positionModifier'))
+    bodyElement.style.setProperty('--positionModifier', Number(pageIndexModifier) + Number(modifier));
+
+    currentPage.classList.remove("selectedPage")
+    let getSibling = () => {if (modifier == -1) return "previousElementSibling"
+        else return "nextElementSibling"
+    }
+    console.log(currentPage[getSibling()])
+    currentPage[getSibling()].classList.add("selectedPage")
+
+})
+}
+
+let createNoteFunctionality = () => {
+    let notes = document.querySelectorAll(".testerInstance")
+    console.log(notes)
+
+    let openNote = (note) => {
+        note.currentTarget.classList.add("selectedNote")
+        note.currentTarget.removeEventListener("click",openNote)
+        note.currentTarget.addEventListener("click",closeNote)
+
+    }
+
+    let closeNote = (note) => {
+        note.currentTarget.classList.remove("selectedNote")
+        note.currentTarget.removeEventListener("click",closeNote)
+        note.currentTarget.addEventListener("click",openNote)
+
+    }
+    notes.forEach((note) => {
+        note.addEventListener("click" ,openNote)
+    })
+}
+
+createNoteFunctionality()
+
+setMovementButton("prevPageButton", -1)
+setMovementButton("nextPageButton",1)
+}
+setCSS()
+
+
 setKeyMovement()
 addMainDumpEventListener(contentText)
+
